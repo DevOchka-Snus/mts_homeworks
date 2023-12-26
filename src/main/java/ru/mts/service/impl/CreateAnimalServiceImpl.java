6e42614d566f522b981db3020a9c63ee0379ed60
vladi <vladislav_fedotov_official@bk.ru>
2023-12-26 12:38:03 +0300

    HW-4

--- src/main/java/ru/mts/service/impl/CreateAnimalServiceImpl.java
+++ src/main/java/ru/mts/service/impl/CreateAnimalServiceImpl.java
@@ -1,18 +1,5 @@
 package ru.mts.service.impl;
 
-import ru.mts.domain.AbstractAnimal;
-import ru.mts.domain.Animal;
-import ru.mts.domain.pet.Cat;
-import ru.mts.domain.pet.Dog;
-import ru.mts.domain.predator.Lion;
-import ru.mts.domain.predator.Wolf;
 import ru.mts.service.CreateAnimalService;
-
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
 
 public class CreateAnimalServiceImpl implements CreateAnimalService {}
